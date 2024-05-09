aec9d30803f4c318926762fa84176d1d542a4a85
Rocky Dey <coderrocky25@gmail.com>
2024-05-09 20:52:12 +0600

    initialize new project called restaurant management and install all essential

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      color1: "#0c0b09",
+      color2: "#1A1814",
+      color3: "#737373",
+      color4: "#aaaaaa",
+      color5: "#D0D0D0",
+      color6: "#E8E8E8",
+      color7: "#F3F3F3",
+      color8: "#FFFFFF",
+      color9: "#FEA116",
+    },
+    fontFamily: {
+      nunito: '"Nunito", sans-serif',
+      pacifico: '"Pacifico", cursive',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
