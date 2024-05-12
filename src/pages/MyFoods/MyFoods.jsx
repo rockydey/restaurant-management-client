e9eb28073c02f2b7a8fcf6642c4d8b293cc97106
Rocky Dey <coderrocky25@gmail.com>
2024-05-12 17:37:21 +0600

    update food form design complete

--- src/pages/MyFoods/MyFoods.jsx
+++ src/pages/MyFoods/MyFoods.jsx
@@ -1,10 +1,12 @@
 import { Helmet } from "react-helmet-async";
 import loginBg from "../../assets/login/login-hero.jpg";
 import { useContext, useEffect, useState } from "react";
+
 import { AuthContext } from "../../provider/AuthProvider/AuthProvider";
 import axios from "axios";
 import { GrUpdate } from "react-icons/gr";
 import {
+  Modal,
   Table,
   TableBody,
   TableCell,
@@ -16,6 +18,9 @@ import {
 const MyFoods = () => {
   const { user } = useContext(AuthContext);
   const [myFoods, setMyFoods] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+  const [updateList, setUpdateList] = useState([]);
 
   useEffect(() => {
     axios
@@ -23,6 +28,33 @@ const MyFoods = () => {
       .then((res) => setMyFoods(res.data))
       .catch((error) => console.error(error.message));
   }, [user]);
+
+  useEffect(() => {
+    setUpdateList(myFoods.find((food) => food._id === updateId));
+  }, [updateId]);
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const fName = form.fName.value;
+    const fPrice = form.fPrice.value;
+    const fQuantity = form.fQuantity.value;
+    const fImage = form.fImage.value;
+    const fCategory = form.fCategory.value;
+    const fOrigin = form.fOrigin.value;
+    const fDescription = form.fDescription.value;
+
+    const updateFood = {
+      food_name: fName,
+      food_category: fCategory,
+      price: fPrice,
+      quantity: fQuantity,
+      origin: fOrigin,
+      description: fDescription,
+      food_image: fImage,
+    };
+    console.log(updateFood);
+  };
 
   return (
     <div>
@@ -48,11 +80,12 @@ const MyFoods = () => {
             <div className='lg:w-4/5 mx-auto'>
               <div className='overflow-x-auto'>
                 <Table hoverable>
-                  <TableHead className='bg-color7 text-color2 font-semibold text-sm'>
+                  <TableHead className='bg-color7 border-b border-color3 text-color2 font-semibold text-sm'>
                     <TableHeadCell>Food Image</TableHeadCell>
                     <TableHeadCell>Food Name</TableHeadCell>
                     <TableHeadCell>Price</TableHeadCell>
                     <TableHeadCell>Category</TableHeadCell>
+                    <TableHeadCell>Origin</TableHeadCell>
                     <TableHeadCell>Action</TableHeadCell>
                   </TableHead>
                   <TableBody className='divide-y'>
@@ -62,7 +95,7 @@ const MyFoods = () => {
                         key={myFood._id}>
                         <TableCell>
                           <img
-                            className='md:w-20 md:h-20'
+                            className='lg:w-20 lg:h-20'
                             src={myFood.food_image}
                             alt=''
                           />
@@ -76,8 +109,16 @@ const MyFoods = () => {
                         <TableCell className='text-lg text-color2 font-medium'>
                           {myFood.food_category}
                         </TableCell>
+                        <TableCell className='text-lg text-color2 font-medium'>
+                          {myFood.origin}
+                        </TableCell>
                         <TableCell>
-                          <button className='flex text-xl bg-color9 text-color8 p-2 rounded-full'>
+                          <button
+                            onClick={() => {
+                              setOpenModal(!openModal);
+                              setUpdateId(myFood._id);
+                            }}
+                            className='flex text-xl bg-color9 text-color8 p-2 rounded-full'>
                             <GrUpdate />
                           </button>
                         </TableCell>
@@ -89,6 +130,142 @@ const MyFoods = () => {
             </div>
           </div>
         </div>
+        <Modal
+          show={openModal}
+          size='xl'
+          popup
+          onClose={() => setOpenModal(false)}>
+          <Modal.Header className='bg-color7 border-b-0' />
+          <Modal.Body className='bg-color7 rounded-b'>
+            <div className='space-y-6'>
+              <h3 className='text-2xl text-color9 font-pacifico text-center font-medium '>
+                Update Your Food
+              </h3>
+              <form onSubmit={handleUpdate} className='space-y-3'>
+                <div className='flex md:flex-row flex-col gap-5 md:gap-0 justify-between'>
+                  <div className='flex flex-col gap-1'>
+                    <label className='font-semibold text-lg' htmlFor='fName'>
+                      Food Name:{" "}
+                    </label>
+                    <input
+                      defaultValue={updateList?.food_name}
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      type='text'
+                      placeholder='Food name'
+                      name='fName'
+                      required
+                      id='fName'
+                    />
+                  </div>
+                  <div className='flex flex-col gap-1'>
+                    <label className='font-semibold text-lg' htmlFor='fPrice'>
+                      Price:{" "}
+                    </label>
+                    <input
+                      defaultValue={updateList?.price}
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      type='text'
+                      placeholder='Food price'
+                      name='fPrice'
+                      id='fPrice'
+                      required
+                    />
+                  </div>
+                </div>
+                <div className='flex md:flex-row flex-col gap-5 md:gap-0 justify-between'>
+                  <div className='flex flex-col gap-1'>
+                    <label
+                      className='font-semibold text-lg'
+                      htmlFor='fQuantity'>
+                      Quantity:{" "}
+                    </label>
+                    <input
+                      defaultValue={updateList?.quantity}
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      type='number'
+                      name='fQuantity'
+                      id='fQuantity'
+                      placeholder='Food quantity'
+                      required
+                    />
+                  </div>
+                  <div className='flex flex-col gap-1'>
+                    <label className='font-semibold text-lg' htmlFor='fImage'>
+                      Food Image:{" "}
+                    </label>
+                    <input
+                      defaultValue={updateList?.food_image}
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      placeholder='Food image url'
+                      type='text'
+                      name='fImage'
+                      id='fImage'
+                      required
+                    />
+                  </div>
+                </div>
+                <div className='flex md:flex-row flex-col gap-5 md:gap-0 justify-between'>
+                  <div className='flex flex-col gap-1'>
+                    <label
+                      className='font-semibold text-lg'
+                      htmlFor='fCategory'>
+                      Category:{" "}
+                    </label>
+                    <input
+                      defaultValue={updateList?.food_category}
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      type='text'
+                      name='fCategory'
+                      id='fCategory'
+                      placeholder='Food Category'
+                      required
+                    />
+                  </div>
+                  <div className='flex flex-col gap-1'>
+                    <label className='font-semibold text-lg' htmlFor='fOrigin'>
+                      Origin:{" "}
+                    </label>
+                    <input
+                      defaultValue={updateList?.origin}
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      placeholder='Food origin'
+                      type='text'
+                      name='fOrigin'
+                      id='fOrigin'
+                      required
+                    />
+                  </div>
+                </div>
+                <div className=''>
+                  <div className='flex flex-col gap-1'>
+                    <label
+                      className='font-semibold text-lg'
+                      htmlFor='fDescription'>
+                      Description:{" "}
+                    </label>
+                    <textarea
+                      defaultValue={updateList?.description}
+                      rows='5'
+                      className='w-full rounded-md focus:shadow-none focus:ring-0 border-color4 px-4 focus:outline-none text-base font-normal text-color2'
+                      type='number'
+                      name='fDescription'
+                      id='fDescription'
+                      placeholder='Food Description'
+                      required
+                    />
+                  </div>
+                </div>
+                <div className='text-center'>
+                  <button
+                    className={`bg-color9 rounded mt-2 text-color8 px-5 py-2 cursor-pointer  font-semibold`}
+                    type='submit'>
+                    Update
+                  </button>
+                </div>
+              </form>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
