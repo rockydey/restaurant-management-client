8365fe7e1b9d52adf46395ebd7f4dcef826931d6
Rocky Dey <coderrocky25@gmail.com>
2024-05-13 22:28:29 +0600

    bottom to top button design and implementation complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,36 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../../components/HomeComponents/Banner/Banner";
 import TopFoods from "../../components/HomeComponents/Banner/TopFoods/TopFoods";
 import AboutUs from "../../components/HomeComponents/AboutUs/AboutUs";
+import "./Home.css";
+import { useEffect, useState } from "react";
+import { IoIosArrowDropup } from "react-icons/io";
 
 const Home = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
       <Helmet>
@@ -12,6 +40,13 @@ const Home = () => {
       <Banner />
       <AboutUs />
       <TopFoods />
+      <button
+        onClick={scrollToTop}
+        className={`p-3 flex items-center justify-center bg-color9 text-color8 font-bold text-3xl fixed bottom-5 right-5 border-0 scroll-btn ${
+          isVisible ? "show" : "hide"
+        }`}>
+        <IoIosArrowDropup />
+      </button>
     </div>
   );
 };
